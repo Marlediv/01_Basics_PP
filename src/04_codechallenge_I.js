3091e0ef9f0764859fbf5d047257e60381426dfd
Marco Grochowiak <marco.grochowiak@outlook.com>
2024-07-02 09:47:09 +0000

    challange gelöst

--- src/04_codechallenge_I.js
+++ src/04_codechallenge_I.js
@@ -10,3 +10,15 @@ Geben Sie die Summe der Zahlen in die Konsole aus:
 */
 
 const prompt = require('prompt-sync')({sigint: true});
+
+// Prompt-Anforderung für die erste Zahl
+let ersteZahl = parseFloat(prompt("Geben Sie die erste Zahl ein:"));
+
+// Prompt-Anforderung für die zweite Zahl
+let zweiteZahl = parseFloat(prompt("Geben Sie die zweite Zahl ein:"));
+
+// Berechnung der Summe der beiden Zahlen
+let summe = ersteZahl + zweiteZahl;
+
+// Ausgabe der Summe in der Konsole
+console.log("Die Summe der Zahlen ist: " + summe);
