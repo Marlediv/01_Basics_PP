c2cf852af74faf3381c9f00bf39f74e2704cb675
Marco Grochowiak <marco.grochowiak@outlook.com>
2024-07-03 07:21:58 +0000

    Iteration 2+3 ++

--- src/05_wahrheitswerte.js
+++ src/05_wahrheitswerte.js
@@ -6,12 +6,28 @@ let ageJohn, ageMark;
 const birthYearJohn = 1980;
 const birthYearMark = 1990;
 
-// Berechnung Alter
-let year = 2024;
-ageJohn = year - birthYearJohn;
-ageMark = year - birthYearMark;
+// // Berechnung Alter Iteration 1
+// let year = 2024;
+// ageJohn = year - birthYearJohn;
+// ageMark = year - birthYearMark;
 
-// Ausgabe
-console.log("ageJohn: " + ageJohn);
-console.log("ageMark: " + ageMark);
+// // Berechnung Alter Iteration 2
+// let date = new Date();
+// let year = date.getFullYear();
+
+// ageJohn = year - birthYearJohn;
+// ageMark = year - birthYearMark;
+
+
+// // Berechnung Alter Iteration 3
+// let year = new Date().getFullYear();
+// ageJohn = year - birthYearJohn;
+// ageMark = year - birthYearMark;
+
+// // Ausgabe
+// console.log("ageJohn: " + ageJohn);
+// console.log("ageMark: " + ageMark);
+
+// console.log("Datum " + date);
+// console.log("Jahr " + year);
 
