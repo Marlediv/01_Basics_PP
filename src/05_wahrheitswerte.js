0e14bf5000dc2d66f5a68e32b6b460f299bafb64
Marco Grochowiak <marco.grochowiak@outlook.com>
2024-07-03 08:04:24 +0000

    update

--- src/05_wahrheitswerte.js
+++ src/05_wahrheitswerte.js
@@ -24,10 +24,15 @@ let year = new Date().getFullYear();
 ageJohn = year - birthYearJohn;
 ageMark = year - birthYearMark;
 
-// Ausgabe
-console.log("ageJohn: " + ageJohn);
-console.log("ageMark: " + ageMark);
+// // Ausgabe
+// console.log("ageJohn: " + ageJohn);
+// console.log("ageMark: " + ageMark);
 
 // Deklaration
 let isJohnOlder; // Is John older? -->  y/n --> true / false
+isJohnOlder = (ageJohn > ageMark); // Test auf Ungleichheit
+let isJohnEqual = (ageJohn == ageMark); // Test auf Gleichheit
 
+// Ausgabe
+console.log("isJohnOlder: " + isJohnOlder);
+console.log("isJohnEqual: " + isJohnEqual);
