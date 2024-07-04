c7d01781d80f90a6ef2ed3e050e0d7822e0d9e1b
Marco Grochowiak <marco.grochowiak@outlook.com>
2024-07-04 15:10:35 +0000

    /* Getränke-Challenge */
    /********* LÖSUNGSVORSCHLAG **********/

--- src/07_codechallenge_II.js
+++ src/07_codechallenge_II.js
@@ -18,3 +18,29 @@ Alter 13-17 // Cola
 
 
 /********* LÖSUNGSVORSCHLAG **********/
+
+const prompt = require('prompt-sync')({sigint: true});
+
+let firstName = prompt("Wie ist Dein Name? Bitte eingeben:...");
+let age = parseInt(prompt("Wie alt bist Du? Bitte eingeben:..."));
+
+switch (true) {
+    case (age >= 0) && (age <= 5): 
+    console.log(firstName + "´s Getränk ist Milch.");
+        break;
+
+    case (age >= 6) && (age <= 12): 
+    console.log(firstName + " hat die Wahl zwischen Milch und Saft.");
+        break;
+
+    case (age >= 13) && (age <= 17): 
+        console.log(firstName + " hat die Wahl zwischen Milch, Saft und Cola.");
+        break;
+
+    case (age >= 18): 
+    console.log(firstName + " hat die Wahl zwischen Milch, Saft, Cola und Wein.");
+        break;
+
+    default: console.log("Programmabbruch wegen Falscheingabe");
+        break;
+}
