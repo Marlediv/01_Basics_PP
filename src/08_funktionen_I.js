0ae8a4f3200835b67fa42b4be0df9fabe34c75e2
Marco Grochowiak <marco.grochowiak@outlook.com>
2024-07-04 08:51:13 +0000

    Funktionen 02a

--- src/08_funktionen_I.js
+++ src/08_funktionen_I.js
@@ -2,3 +2,32 @@
 /***** Funktionen 01 *****/
 // 1. Kapselung von Codeblöcken
 
+// Funktionsaufruf | call
+
+// test();
+
+// Funktionsrumpf | callee
+// Funktionsdeklaration
+
+// function test()
+//     {
+//         console.log("Hallo Welt");
+// }
+
+/***** Funktionen 02a *****/
+// 2a. Parametrisierung + Datenübergabe von INNEN
+
+// Call
+ausgabeName();
+
+// Funktion
+function ausgabeName()
+    {
+
+    //interne Variable | What happens in VEAGS...
+    let firstName = "Sara"
+    console.log("Hallo " + firstName + "!");
+
+}
+
+// console.log(firstName); // Fehler : scope!
