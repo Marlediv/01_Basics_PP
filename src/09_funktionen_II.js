a8fafbd5c7f59f2dde4f2ac8f6c598536d974652
Marco Grochowiak <marco.grochowiak@outlook.com>
2024-07-05 08:43:57 +0000

    Ausgabe in Konole

--- src/09_funktionen_II.js
+++ src/09_funktionen_II.js
@@ -7,17 +7,12 @@
 1. Dateneingabe + -überprüfung :  
 2. Auswahl Rechenart : 
 3. Fkt. Grundrechenarten : 
-4. Ausgabe in Konsole : 
+4. Ausgabe in Konsole : check!
 */
 
 
 
-
-
-
-
-
-// module: output | test:
+// module: output | test: check!
 // output("hello");
 // output(2);
 function output(outputData) {
