0243a7700481c510f926595a38fa539b6f64d979
Marco Grochowiak <marco.grochowiak@outlook.com>
2024-07-05 08:44:28 +0000

    Fkt. Grundrechenarten

--- src/09_funktionen_II.js
+++ src/09_funktionen_II.js
@@ -6,11 +6,52 @@
 
 1. Dateneingabe + -überprüfung :  
 2. Auswahl Rechenart : 
-3. Fkt. Grundrechenarten : 
+3. Fkt. Grundrechenarten : check!
 4. Ausgabe in Konsole : check!
 */
 
+// Fkt. Grundrechenarten
 
+// module: multiplication a * b |  test: check!
+// output(multiply(3,2));
+// output(multiply(3,-2));
+// output(multiply(3,0));
+function multiply(a,b) {
+	return  a * b;
+}
+
+// module: division a / b |  test: check!
+// output(divide(4,2));
+// output(divide(3,2));
+// output(divide(3,-2));
+// output(divide(0,2));
+// output(divide(3,0));
+// output(divide(0,0));
+function divide(a,b) {
+	if (b != 0) {
+		return a / b ;
+	} else {
+		return "Teilen durch 0 nicht möglich!"
+	}
+	
+}
+
+// module: subtraction a - b |  test: check!
+// output(subtract(3,2));
+// output(subtract(3,-2));
+// output(subtract(3,0));
+// output(subtract(0,2));
+function subtract(a,b) {
+	return a - b;
+}
+
+// module: addition a + b |  test: check! 
+// output(add(2,2));
+// output(add(2,-2));
+// output(add(2,0));
+function add(a,b) {
+	return a + b;
+}
 
 // module: output | test: check!
 // output("hello");
